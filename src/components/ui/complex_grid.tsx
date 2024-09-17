6db2386d774d54fb6af44d0739454a57aeed8d2f
Ali Raza <alirazadev27@gmail.com>
2024-09-17 12:05:16 +0500

    UI/UX improve V5

--- src/components/ui/complex_grid.tsx
+++ src/components/ui/complex_grid.tsx
@@ -27,19 +27,19 @@ export function ComplexGrid(){
     return(
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-8 gap-16 px-4 py-8">
             <div className="flex flex-col justify-between gap-8 space-y-8">
-                <h2 className="text-primary font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Top Selling</h2>
+                <h2 className="text-neutral-300 font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Top Selling</h2>
                 {topSelling.map((card:any,index:number)=> <div className="gridItem" key={index}><SmallCard  data={card}/></div>)}   
             </div>
             <div className="flex flex-col justify-between  gap-8 space-y-8">
-                <h2 className="text-primary font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Trending</h2>
+                <h2 className="text-neutral-300 font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Trending</h2>
                 {trending.map((card:any,index:number)=> <div className="gridItem" key={index}><SmallCard key={index} data={card}/></div>)}   
             </div>
             <div className="flex flex-col justify-between  gap-8 space-y-8">
-                <h2 className="text-primary font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Recently Added</h2>
+                <h2 className="text-neutral-300 font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Recently Added</h2>
                 {recentlyAdded.map((card:any,index:number)=> <div className="gridItem" key={index}><SmallCard  data={card}/></div>)}   
             </div>
             <div className="flex flex-col justify-between gap-8 space-y-8">
-                <h2 className="text-primary font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Top Rated</h2>
+                <h2 className="text-neutral-300 font-bold text-2xl underline underline-offset-[16px] decoration-green-300 decoration-3">Top Rated</h2>
                 {topRated.map((card:any,index:number)=> <div className="gridItem" key={index}><SmallCard  data={card}/></div>)}   
             </div>
         </div>
--- src/components/ui/deals_of_the_day.tsx
+++ src/components/ui/deals_of_the_day.tsx
@@ -1,15 +1,36 @@
 import { deals } from "../../../data"
 import { DealCard } from "./deal_card"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 export function DealsOfTheDay(){
+    gsap.registerPlugin(useGSAP,ScrollTrigger);
+    useGSAP(() => {
+        gsap.fromTo(".deal",{x:function(index,_,targets){
+            if(index<targets.length/2){ return `-100vw`;}
+            else{return `100vw`;}}},
+            {
+                x:0,
+                duration:4,
+                ease:"bounce.out",
+                repeat:0,
+                scrollTrigger:{
+                    trigger:'.deal',
+                    start:"center 70%",
+                    toggleActions:"play none none none",
+                }
+
+            })
+    })
     return(
-        <div className="py-8 px-4 md:mt-16">
+        <div className="py-8 px-4 md:mt-16 overflow-hidden">
             <div className="flex justify-between items-center">
                 <h1 className="text-neutral-300 text-3xl font-bold">Deals Of The Day</h1>
                 <p className="text-neutral-400 hover:bg-lightgreen hover:text-white px-4 py-2 rounded-3xl transition-all duration-500">All Deals</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 pt-8 ">
         {
-            deals.map((deal,index)=> <DealCard key={index} data={deal}/>)
+            deals.map((deal,index)=> <div key={index} className="deal"><DealCard  data={deal}/></div>)
         }
             </div>
         </div>
