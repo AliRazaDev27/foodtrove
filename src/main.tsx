5ee69fae3ba10d7e0246e5344c0cd068eddc36a4
Ali Raza <alirazadev27@gmail.com>
2024-09-20 18:46:34 +0500

    Partial dynamic imports

--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import {Provider} from "react-redux"
 import store from "./store"
 
-const Root = lazy(()=>import('./pages/root.tsx'))
-const Home = lazy(()=>import('./pages/home.tsx'))
+import Root from './pages/root.tsx'
+import Home from './pages/home.tsx'
+import ShopLayout from './pages/shop_layout.tsx'
+import Products from './pages/products.tsx'
+import About from './pages/about.tsx'
+import Blog from './pages/blog.tsx'
 const Register = lazy(()=>import('./pages/register.tsx'))
 const Login = lazy(()=>import('./pages/login.tsx'))
 const Seed = lazy(()=>import('./pages/seed.tsx'))
-const Products = lazy(()=>import('./pages/products.tsx'))
 const ProductDetails = lazy(()=>import('./pages/product_details.tsx'))
-const ShopLayout = lazy(()=>import('./pages/shop_layout.tsx'))
 const Cart = lazy(()=>import('./pages/cart.tsx'))
 const Checkout = lazy(()=>import('./pages/checkout.tsx'))
-const About = lazy(()=>import('./pages/about.tsx'))
-const Blog = lazy(()=>import('./pages/blog.tsx'))
 const Wishlist = lazy(()=>import('./pages/wishlist.tsx'))
 const Test = lazy(()=>import('./pages/test.tsx'))
 
@@ -100,7 +100,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
     <Provider store={store}>
-      <Suspense fallback={null}>
+      <Suspense fallback={<div>Loading...</div>}>
     <RouterProvider router={router}/>
     </Suspense>
     </Provider>
