357be4d97bfc848bde23d16efb28af6e1ee073e2
Ali Raza <alirazadev27@gmail.com>
2024-09-28 10:49:54 +0500

    Added: cart items length preview in header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -11,11 +11,11 @@ import {
   import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { WaterFallBorder } from "../waterfall_border";
-
 //this may be improved
 
 export default function Header() {
   const user = useSelector((state:any) => state.auth.user)
+  const cartLength = useSelector((state:any) => state.cart.items.length)
   const location = useLocation()
   return (
     <>
@@ -74,18 +74,24 @@ export default function Header() {
             <span>Wishlist</span></Link>
           </li>
           <li>
+            <div className="relative">
             <Link to="/cart" className="flex gap-2 items-center hover:underline underline-offset-8 decoration-[2px] decoration-lightred">
             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="176" cy="416" r="16" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></circle><circle cx="400" cy="416" r="16" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></circle><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M48 80h64l48 272h256"></path><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128"></path></svg>
              <span>Cart</span>
              </Link>
+             {cartLength > 0 && <span className="absolute top-0 right-0 translate-x-[90%] -translate-y-[90%]  text-sm px-1 bg-black text-white flex items-center justify-center rounded-full">{cartLength}</span>}
+            </div>
           </li>
         </ul>
         :
         <div className="flex gap-4 md:gap-6  items-center">
+          <div className="relative">
           <Link to="/cart" className="flex gap-2 items-center hover:underline underline-offset-8 decoration-[2px] decoration-lightred">
-          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"></path></svg>
+          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="176" cy="416" r="16" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></circle><circle cx="400" cy="416" r="16" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"></circle><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M48 80h64l48 272h256"></path><path fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M160 288h249.44a8 8 0 0 0 7.85-6.43l28.8-144a8 8 0 0 0-7.85-9.57H128"></path></svg>
            <span>Cart</span>
            </Link>
+             {cartLength > 0 && <span className="absolute top-0 right-0 translate-x-[90%] -translate-y-[90%]  text-sm px-1 bg-black text-white flex items-center justify-center rounded-full">{cartLength}</span>}
+           </div>
           <Link to="/login"  className="rounded-2xl bg-black/90 hover:bg-black text-white px-4 py-2">Login</Link>
         </div>
         }
